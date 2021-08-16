69d3ee14889ed92be0ec1ac130ed7d78dec5ac5a
Makiyu <73825066+Makiyu-py@users.noreply.github.com>
2021-08-16 16:07:18 +0800

    fixed parsing of game mode strings on chess.com component

--- src/chess-com.svelte
+++ src/chess-com.svelte
@@ -1,31 +1,34 @@
 <script lang="ts">
-    import { fetchBase } from "./fetcher";
+  import { fetchBase } from "./fetcher";
 
-    export let name: string;
-    
-    const chessComRes = fetchBase({
-      url: `https://api.chess.com/pub/player/${name}/stats`,
-    });
-  </script>
-  
-  <main>
-    {#await $chessComRes}
-      <h1>Loading chess.com data...</h1>
-    {:then data}
-      <h1>{name}'s chess.com Ratings</h1>
-      <ul>
-        {#each Object.entries(data) as chessModes}
-          {#if "last" in chessModes[1]}
-            <li>{chessModes[0].slice(6)}: {chessModes[1].last.rating}</li>
-          {/if}
-        {/each}
-      </ul>
-    {/await}
-  </main>
-  
-  <style>
-    li {
-      text-transform: capitalize;
-    }
-  </style>
-  
+  export let name: string;
+
+  const chessComRes = fetchBase({
+    url: `https://api.chess.com/pub/player/${name}/stats`,
+  });
+</script>
+
+<main>
+  {#await $chessComRes}
+    <h1>Loading chess.com data...</h1>
+  {:then data}
+    <h1>{name}'s chess.com Ratings</h1>
+    <ul>
+      {#each Object.entries(data) as chessModes}
+        {#if "last" in chessModes[1]}
+          <li>
+            {chessModes[0].startsWith("chess_")
+              ? chessModes[0].slice(6)
+              : chessModes[0].split("_").join(" ")}: {chessModes[1].last.rating}
+          </li>
+        {/if}
+      {/each}
+    </ul>
+  {/await}
+</main>
+
+<style>
+  li {
+    text-transform: capitalize;
+  }
+</style>
