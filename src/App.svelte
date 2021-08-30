29595d2f0d936cc9b810e24a91d3f1019408f5dc
Makiyu-py <73825066+Makiyu-py@users.noreply.github.com>
2021-08-30 17:43:25 +0800

    i hope this finally fixes the meta tag problem

--- src/App.svelte
+++ src/App.svelte
@@ -18,26 +18,6 @@
 	];
 </script>
 
-<svelte:head>
-	<meta property="og:site_name" content="Chess Stats" />
-	<meta property="og:title" content="Chess Stats" />
-	<meta
-		property="og:description"
-		content="Display your online chess stats on the web."
-	/>
-	<meta property="og:type" content="website" />
-	<meta
-		property="og:url"
-		content="https://makiyu-py.github.io/chess-stats/"
-	/>
-	<meta
-		property="og:image"
-		content="https://twemoji.maxcdn.com/v/latest/72x72/265f.png"
-	/>
-	<meta name="theme-color" content="#32373d" />
-	<title>Chess Stats</title>
-</svelte:head>
-
 <main>
 	<button on:click={() => (curPage = curPage === 0 ? 1 : 0)}
 		>Switch to {notPageName}</button
